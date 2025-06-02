4e91cb7f0349cc51db699182f5c0103fb3e32569
Ikhsan Fillah Hidayat <ikhsanfillahhidayat@gmail.com>
2025-06-02 22:25:16 +0700

    mengganti nama

---
+++ src/tokolaptop/controllers/LaptopController.java
@@ -0,0 +1,23 @@
+package tokolaptop.controllers;
+
+import java.util.List;
+import tokolaptop.helper.DBHelper;
+import tokolaptop.models.Laptop;
+
+public class LaptopController {
+    public static List<Laptop> getAll(){
+        return DBHelper.getAllLaptops();
+    }
+    
+    public static boolean insert(Laptop l){
+        return DBHelper.insertLaptop(l);
+    }
+    
+    public static boolean update(Laptop l){
+        return DBHelper.updateLaptop(l);
+    }
+    
+    public static boolean delete(String id){
+        return DBHelper.deleteLaptop(id);
+    }
+}
